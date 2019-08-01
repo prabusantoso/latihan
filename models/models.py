02b0327271b0d9eccfa035e8e5723b356b90ee97
prabusantoso <prabusantoso95@gmail.com>
2019-08-01 17:01:30 +0700

    onchange

--- models/models.py
+++ models/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
-
+from odoo import models, fields, api, exceptions
 
 # class training_odoo(models.Model):
 #     _name = 'training_odoo.training_odoo'
@@ -24,6 +23,28 @@ class Kursus(models.Model):
     session_ids = fields.One2many('training.sesi', 'course_id', string="Sesi")
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Penanggung Jawab", index=True)
     
+    _sql_constraints = [
+                    ('name_description_cek', 'CHECK(name != description)', 'Judul kursus dan keterangan tidak boleh sama '),
+                    ('name_unik', 'UNIQUE(name)', 'Judul kursus harus unik')
+    ]
+    
+    
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+        copied_count = self.search_count([('name', '=like', "Copy of {}%".format(self.name))]) # Searching judul kursus yang sama dan menyimpannya pada variable copied_count 
+        
+        if not copied_count: # Cek isi variable copied_count (hasil searching) 
+            new_name = "Copy of {}".format(self.name) # Jika proses searching judul yang sama tidak ditemukan, maka judul baru akan dikasih imbuhan 'Copy of'
+        else:
+            new_name = "Copy of {} ({})".format(self.name, copied_count) # Jika proses searching judul yang sama ditemukan, maka judul baru akan dikasih imbuhan 'Copy of' dan angka terakhir duplicate
+        
+        default['name'] = new_name # Mereplace value field name dengan yang sudah di sesuaikan
+        return super(Kursus, self).copy(default)
+
+
+
+    
 class Sesi(models.Model):
     _name = 'training.sesi'
     
@@ -40,7 +61,46 @@ class Sesi(models.Model):
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         for r in self:
-            if not r.seats:
-                r.taken_seats = 0.0
-            else:
-                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+            if not r.seats: # cek nilai field seats, jika 0 (false), maka masuk kondisi if 
+                r.taken_seats = 0.0 # update field taken_seats menjadi 0
+            else: # jika tidak masuk kondisi if
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats # update field taken_seats menjadi persentase dari jumlah peserta & kursi
+    
+                    
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats <= 0: # cek nilai field seats, jika dibawah 0 (negatif), maka masuk kondisi if
+            return {
+                    'value': {
+                              'seats': len(self.attendee_ids) or 1 # mengisi field seats dengan nilai jumlah peserta atau 1
+                              },
+                    'warning': {
+                                'title': "Nilai Jumlah Kursi Salah", # judul pop up
+                                'message': "Jumlah Kursi Tidak Boleh Negatif" # pesan pop up
+                                }
+                    }
+            
+        if self.seats < len(self.attendee_ids): # cek nilai field seats (jumlah kursi) apakah lebih kecil dari field attendee_ids (jumlah peserta), jika iya maka masuk kondisi if
+            return {
+                    'value': {
+                              'seats': len(self.attendee_ids) # mengisi field seats dengan nilai jumlah peserta
+                              },
+                    'warning': {
+                                'title': "Peserta Terlalu Banyak", # judul pop up
+                                'message': "Tambahkan Kursi atau Kurangi Peserta" # pesan pop up
+                                }
+                    }
+            
+                
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids: # Jika field instructor_id (Instruktur) diisi DAN instructor_id ada di tabel attendee_ids (Peserta), maka munculkan pesan error 
+                raise exceptions.ValidationError("Seorang instruktur tidak boleh menjadi peserta")
+    
+
+
+    
+    
+
+    
